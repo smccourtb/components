8e5b509a3f57c689e9f26e9a00510ad6107df60e
Shawnathan McCourt <shawnmccourtb@gmail.com>
2023-05-11 00:05:18 -0400

    remove copy to clipboard function

--- utils/index.ts
+++ utils/index.ts
@@ -5,11 +5,3 @@ export const isOverflown = (ref: React.RefObject<HTMLElement>) => {
   const { clientHeight, scrollWidth, scrollHeight, clientWidth } = ref.current;
   return scrollHeight > clientHeight || scrollWidth > clientWidth;
 };
-
-// TODO: safari copies the entire element, not just the class name
-export const copyClassNameToClipboard = (e: React.MouseEvent) => {
-  const element = e.target as HTMLElement;
-  navigator.clipboard
-    .writeText(element.className)
-    .then((r) => console.log("copied class name to clipboard", r));
-};
